e4458ad377a8761f6eb8c17faaede4d8acc84d46
Ayush <kumar.ayush151@gmail.com>
2025-06-17 18:30:44 +0530

    promise polyfill

---
+++ js-probs/promises/promisePolyfill.js
@@ -0,0 +1,29 @@
+/*  */
+
+new Promise((resolve, reject) => {
+    /* 
+    some things and resolve()
+     */
+
+    /* 
+    some things and then reject()
+    */
+})
+
+
+class PromisePolyfill{
+
+    constructor() {
+
+    }
+
+    resolvePf = () => {
+
+    }
+
+    rejectPf = () => {
+        
+    }
+
+
+}
