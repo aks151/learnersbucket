e94fe4c0730902923ec2b2a129a8ee6fe51be686
Ayush-Pers <ayush-pers@Ayushs-9.local>
2025-06-17 21:29:35 +0530

    Design Pattern Folder Added

---
+++ design-patterns/observer.js
@@ -0,0 +1,16 @@
+/* 
+this is also known as pub-sub design pattern
+host <> observer
+
+a subscription model where the object subscribes to a host and the host informs the object whenever an event occurs is observer pattern
+
+v important part of event driven paradigm
+
+this pattern promotes loose coupling facilitating good object oriented design.
+
+event handlers are notified whenever the events they are handling occurs and thus follow observer pattern
+*/
+
+/* 
+
+*/
